2a301e772d02cc49678b0baaf30351443f651e3c
Joseph Elias <jrelias@outlook.com>
2022-02-08 18:37:43 -0500

    added rating form submission and animations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -9,6 +11,11 @@ import feedbackData from './data/feedbackData';
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -19,7 +26,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
